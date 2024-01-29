4fb012d5cd2a565a9b7a0b53f90aea8d6571f467
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2024-01-29 15:52:30 +0100

    fix: got the application to run in dev mode successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
--- src/app/protected/protected.component.ts
+++ src/app/protected/protected.component.ts
@@ -1,5 +1,4 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
